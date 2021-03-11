27010b9da788e95e197ed3318cb84ae5af4184b6
Gustavo Silva <gstvds@icloud.com>
2021-03-11 00:21:26 -0300

    feat: Finishes application with all tests passing

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,25 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (newTaskTitle === '') {
+      return;
+    }
+    const id = Math.random() * 10;
+    setTasks([...tasks, { id, title: newTaskTitle, isComplete: false }]);
+    setNewTaskTitle('');
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    setTasks(tasks.map((task) => {
+      if (task.id === id) {
+        return { ...task, isComplete: true };
+      }
+      return task
+    }));
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    setTasks(tasks.filter((task => task.id !== id)));
   }
 
   return (
